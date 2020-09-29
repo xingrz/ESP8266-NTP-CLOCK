52830088ca55dc613e08e363c8322275b9103314
XiNGRZ <chenxingyu92@gmail.com>
2020-09-29 21:45:20 +0800

    clock: hspi: Add hspi_write

--- project/clock/main/hspi.c
+++ project/clock/main/hspi.c
@@ -20,11 +20,11 @@ hspi_init(void)
 	ESP_LOGV(TAG, "hspi_init exit");
 }
 
-static inline void
-hspi_write_item(uint32_t data, uint32_t len)
+void
+hspi_write(void *buf, uint32_t len)
 {
 	spi_trans_t trans = {
-			.mosi = &data,
+			.mosi = buf,
 			.bits = {.mosi = len * 8},
 	};
 
@@ -34,17 +34,17 @@ hspi_write_item(uint32_t data, uint32_t len)
 void
 hspi_write8(uint8_t val)
 {
-	hspi_write_item(val, sizeof(uint8_t));
+	hspi_write(&val, sizeof(uint8_t));
 }
 
 void
 hspi_write16(uint16_t val)
 {
-	hspi_write_item(val, sizeof(uint16_t));
+	hspi_write(&val, sizeof(uint16_t));
 }
 
 void
 hspi_write32(uint32_t val)
 {
-	hspi_write_item(val, sizeof(uint32_t));
+	hspi_write(&val, sizeof(uint32_t));
 }
--- project/clock/main/hspi.h
+++ project/clock/main/hspi.h
@@ -3,8 +3,11 @@
 
 #include "common.h"
 
+#define HSPI_MAX_LEN 64
+
 void hspi_init(void);
 
+void hspi_write(void *buf, uint32_t len);
 void hspi_write8(uint8_t val);
 void hspi_write16(uint16_t val);
 void hspi_write32(uint32_t val);
